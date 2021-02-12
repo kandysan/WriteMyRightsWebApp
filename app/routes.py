5b5301a82575b8caa6299c322bdabc371afc3682
nathan-broyles <55458627+nathan-broyles@users.noreply.github.com>
2021-02-11 17:30:21 -0800

    Linking HTML pages

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template("index.html", title='Write My Rights')
+
+
+@app.route('/writemyrights')
+def write_my_rights_info():
+    return render_template("/writemyrights.html", title="Write My Rights Info")
