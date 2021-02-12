82b9365c329c06f057d28d22374b3797249318a8
nathan-broyles <55458627+nathan-broyles@users.noreply.github.com>
2021-02-11 16:41:58 -0800

    Working on Flask Functionality
    __init__
    routes
    main app

---
+++ WriteMyRights.py
@@ -0,0 +1,10 @@
+from app import app
+# from app import app, getApp
+#
+#
+# @app.shell_context_processor
+# def make_shell_context():
+#     return None
+#
+#
+# app = getApp()
---
+++ app/app.py
@@ -0,0 +1,22 @@
+import connexion
+from connexion import NoContent
+from app import letter_script
+
+
+def process_employment_answers(body):
+    start_employment_script(body)
+    return NoContent, 201
+
+
+def start_employment_script(body):
+    letter = letter_script.create_employment_letter(body)
+    print(letter)
+    return letter
+
+
+# app = connexion.FlaskApp(__name__, specification_dir='')
+# app.add_api("../wmr.yml", strict_validation=True, validate_responses=True)
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
