08498b2ef6978b341a3fef2d1a724a20ee9d479e
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2021-02-12 11:33:19 -0800

    Updated Database to store only the required values

--- Storage/create_tables_mysql.py
+++ Storage/create_tables_mysql.py
@@ -1,20 +1,17 @@
 import mysql.connector
-db_conn = mysql.connector.connect(host="localhost", user="root", password="P@ssw0rd", database="events")
+db_conn = mysql.connector.connect(host="localhost", user="root", password="P@ssw0rd", database="WMRdb")
 db_cursor = db_conn.cursor()
 
+
+# must change address to geo location later
 db_cursor.execute('''
-          CREATE TABLE employment_answers
+          CREATE TABLE users
           (id INT NOT NULL AUTO_INCREMENT, 
            name VARCHAR(250) NOT NULL,
-           address VARCHAR(250) NOT NULL,
-           company_name VARCHAR(250) NOT NULL,
-           company_address VARCHAR(250) NOT NULL,
-           boss_name VARCHAR(250) NOT NULL,
-           time_worked VARCHAR(250) NOT NULL,
-           severance VARCHAR(250) NOT NULL,
+           geo_location VARCHAR(250) NOT NULL,
            email VARCHAR(250) NOT NULL,
            date_created VARCHAR(100) NOT NULL,
-           CONSTRAINT pickup_pk PRIMARY KEY (id))
+           CONSTRAINT users_pk PRIMARY KEY (id))
           ''')
 
 db_conn.commit()
--- Storage/drop_tables_mysql.py
+++ Storage/drop_tables_mysql.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db_conn = mysql.connector.connect(host="localhost", user="root",
-                                  password="P@ssw0rd", database="events")
+                                  password="P@ssw0rd", database="WMRdb")
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
-    DROP TABLE employment_answers
+    DROP TABLE users
     ''')
 
 db_conn.commit()
