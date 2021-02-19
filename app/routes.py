9a134652f050dc7dd873c633b70b723903c9a4e7
Aiden-Lonquist <55508988+Aiden-Lonquist@users.noreply.github.com>
2021-02-18 17:28:35 -0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect, flash
 
+ans_dict = {}
 
 @app.route('/')
 @app.route('/index')
@@ -13,6 +14,30 @@ def write_my_rights_info():
     return render_template("/letterType.html", title="Write My Rights Letter Type")
 
 
-@app.route('/question1')
-def question_1():
-    return render_template("/questions/questionOne.html", title="Question 1")
+# @app.route('/question1')
+# def question_1():
+#     return render_template("/questions/questionOne.html", title="Question 1")
+
+@app.route('/answer', methods=['GET', 'POST'])
+def process_answer():
+    error = ''
+    try:
+        if request.method == 'POST':
+            key = request.form['key']
+            attempted_value = request.form['value']
+
+            if type(attempted_value) == str:
+                ans_dict[key] = attempted_value
+                print(ans_dict)
+                #return redirect(url_for('next_question'))
+                return "data stored", 201
+            else:
+                error = "Invalid answer provided for name. Try Again"
+
+        #return render_template("this_question", error = error)
+        return error, 400
+
+    except Exception as e:
+        pass
+        #return render_template("this_question", error = error)
+        return error, 400
