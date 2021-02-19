0b8074b72f77a859b925a32d869fa49c408abb60
Kevin J. Dabu <55458444+kdabu00@users.noreply.github.com>
2021-02-19 12:52:55 -0800

    API Routing

--- app/routes.py
+++ app/routes.py
@@ -14,30 +14,42 @@ def write_my_rights_info():
     return render_template("/letterType.html", title="Write My Rights Letter Type")
 
 
+@app.route('/questions/questionOne')
+def question1():
+    return render_template("/questions/questionOne.html", title="Question One")
+
 # @app.route('/question1')
 # def question_1():
 #     return render_template("/questions/questionOne.html", title="Question 1")
 
+
 @app.route('/answer', methods=['GET', 'POST'])
-def process_answer():
+def answer():
     error = ''
+    print('AAHHHHHHHHHHHHHHHHHHHHHH')
     try:
+        print('UGHHHHHHHHHHHHHHHHHH')
+        print(request.method)
         if request.method == 'POST':
+
             key = request.form['key']
-            attempted_value = request.form['value']
+            attempted_value = request.form['answer']
+            next_page = request.form['next_page']
+            # '#trying with json instead of form'
+            # key = request.json['key']
+            # attempted_value = request.json['value']
+            # next_page = request.json['next_page']
 
             if type(attempted_value) == str:
                 ans_dict[key] = attempted_value
                 print(ans_dict)
-                #return redirect(url_for('next_question'))
-                return "data stored", 201
+                return redirect(next_page)
             else:
                 error = "Invalid answer provided for name. Try Again"
 
         #return render_template("this_question", error = error)
-        return error, 400
-
+        return error, 401
     except Exception as e:
-        pass
+        print(e)
         #return render_template("this_question", error = error)
-        return error, 400
+        return 402
