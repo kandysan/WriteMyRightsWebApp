23aa0aa1588d8230f86a9f53ee4d26fcac3ee0b3
nathan-broyles <55458627+nathan-broyles@users.noreply.github.com>
2021-02-18 17:25:07 -0800

    Initial routes
    set up initial pages and routes

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ def index():
     return render_template("index.html", title='Write My Rights')
 
 
-@app.route('/writemyrights')
+@app.route('/letterType')
 def write_my_rights_info():
-    return render_template("/writemyrights.html", title="Write My Rights Info")
+    return render_template("/letterType.html", title="Write My Rights Letter Type")
+
+
+@app.route('/question1')
+def question_1():
+    return render_template("/questions/questionOne.html", title="Question 1")
