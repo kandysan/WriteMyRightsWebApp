27c16b948210bac735c8b8b574ef6198944c19ce
nathan-broyles <55458627+nathan-broyles@users.noreply.github.com>
2021-04-08 14:12:16 -0700

    Webhook setup

--- app/emailer.py
+++ app/emailer.py
@@ -12,8 +12,8 @@ from email import encoders
 
 class Email:
 
-    def __init__(self, to_addr: str = "nathancbroyles@gmail.com", file_name: str = "email1.docx"):
-        self.from_addr = "noreply.writemyrights@gmail.com"
+    def __init__(self, to_addr: str = "letters@writemyrights.com", file_name: str = "email1.docx"):
+        self.from_addr = "letters@writemyrights.com"
         self.to_addr = to_addr
         self.file_name = file_name
 
@@ -33,7 +33,7 @@ class Email:
         # string to store the body of the mail
         body = "Thanks for using Write my Rights. Our mission is to help you to " \
                "communicate so you can start solving an everyday legal problem. Your " \
-               "(free)(premium) letter is attached. Keep fighting the good fight. " \
+               " letter is attached. Keep fighting the good fight. " \
                "Like what you see? Get 10% off your next premium letter by using the promo code 10OFF."
 
         # attach the body with the msg instance
@@ -60,13 +60,13 @@ class Email:
         msg.attach(p)
 
         # creates SMTP session
-        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s = smtplib.SMTP('smtp.hostinger.com', 587)
 
         # start TLS for security
         s.starttls()
 
         # Authentication
-        s.login(self.from_addr, "writemyrights!")
+        s.login(self.from_addr, "Totempole85%")
 
         # Converts the Multipart msg into a string
         text = msg.as_string()
